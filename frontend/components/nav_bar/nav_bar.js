611d9b09e3502946a48f1026ea331f7cb2ca471e
Heiman Chan <heimanchan0@gmail.com>
2019-02-27 16:12:53 -0800

    Prepare for initial Heroku push

--- frontend/components/nav_bar/nav_bar.js
+++ frontend/components/nav_bar/nav_bar.js
@@ -13,8 +13,8 @@ import { Link } from 'react-router-dom';
       <div>
         {/* <Link to="/signup">Sign up</Link>
         <Link to="/login">Log in</Link> */}
-        <button className="nav-button" onClick={() => openModal('Log In')}>Log in</button>
         <button className="nav-button" onClick={() => openModal('Sign Up')}>Sign up</button>
+        <button className="nav-button" onClick={() => openModal('Log In')}>Log in</button>
       </div>
     );
 
