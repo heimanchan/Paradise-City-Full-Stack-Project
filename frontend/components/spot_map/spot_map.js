092782aa534c164b46925bcb8af1895ed1f80a97
Heiman Chan <heimanchan0@gmail.com>
2019-03-02 18:38:25 -0800

    Add google map on search page

--- frontend/components/spot_map/spot_map.js
+++ frontend/components/spot_map/spot_map.js
@@ -1,18 +1,29 @@
 import React from 'react';
 
+
+
 class SpotMap extends React.Component {
   constructor(props) {
     super(props);
     this.state;
   }
+  
+  componentDidMount() {
+    // const map = this.refs.map;
+    const mapOptions = {
+      center: { lat: 37.7758, lng: -122.435 }, // this is SF
+      zoom: 13
+    };
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+  }
 
   render() {
     return(
-      <div id="map-container" ref="map">
+      <div id="map-container" ref={map => this.mapNode = map}>
         Map
       </div>
     )
   }
 }
 
-export default SpotMap
+export default SpotMap;
