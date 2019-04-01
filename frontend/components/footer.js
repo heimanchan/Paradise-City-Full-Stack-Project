47aadc622382f2d55d7f301fc0983be62bd013f0
Heiman Chan <heimanchan0@gmail.com>
2019-04-01 15:09:40 -0700

    Add footer to splash page

---
+++ frontend/components/footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="footer">
+      <a className="footer-icon" target="_blank" rel="noopener" href="https://www.linkedin.com/in/dave-chan-04a149165/">
+        <i class="fab fa-linkedin"></i>
+        <div>
+          LinkedIn
+        </div>
+      </a>
+      <a className="footer-icon" target="_blank" rel="noopener" href="https://github.com/heimanchan">
+        <i class="fab fa-github"></i>
+        <div>
+          GitHub
+        </div>
+      </a>
+      <a className="footer-icon" target="_blank" rel="noopener" href="https://heimanchan.github.io/Portfolio-2019/">
+        <i class="fas fa-user"></i>
+        <div>
+          Potfolio
+        </div>
+      </a>
+    </div>
+  )
+}
--- frontend/components/splash.js
+++ frontend/components/splash.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import NavBarContainer from '../components/nav_bar/nav_bar_container';
 import SearchBar from '../components/search_bar';
+import Footer from './footer';
 
 export default () => {
   return (
-    <div className="splash">
-      <NavBarContainer />
-      <div className="splash-box">
-        <div className="fo">
-          <div className="next-trip">
-            <div className="trip-title">
-              <h1>Plan your next trip</h1>
+      <div className="splash">
+        <NavBarContainer />
+        <div className="splash-box">
+          <div className="fo">
+            <div className="next-trip">
+              <div className="trip-title">
+                <h1>Plan your next trip</h1>
+              </div>
+              <SearchBar />
             </div>
-            <SearchBar />
           </div>
         </div>
+        <Footer />
       </div>
-    </div>
   )
 }
 
